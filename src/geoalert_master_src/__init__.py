14069810c09a42d1529741c2338e79474a8e0841
Georgy Potapov <georgy.potapov@gmail.com>
2021-05-25 11:37:32 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !6

--- src/geoalert_master_src/__init__.py
+++ src/geoalert_master_src/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 def classFactory(iface):
+    #
     from .geoalert import Geoalert
     return Geoalert(iface)
--- src/geoalert_master_src/geoalert_dialog.py
+++ src/geoalert_master_src/geoalert_dialog.py
@@ -1,25 +1,20 @@
-from pathlib import Path
+# -*- coding: utf-8 -*-
 
+import os
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog
+from PyQt5 import QtWidgets
 from qgis.core import QgsMapLayerProxyModel
-from qgis.utils import iface
 
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'geoalert_dialog_base.ui'))
 
-FORM_CLASS = uic.loadUiType(Path(__file__).parent/'geoalert_dialog_base.ui')[0]
-
-
-class GeoalertDialog(QDialog, FORM_CLASS):
-    def __init__(self, parent=iface.mainWindow()):
+class GeoalertDialog(QtWidgets.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
         """Constructor."""
         super(GeoalertDialog, self).__init__(parent)
         self.setupUi(self)
-        # Adjust column width in processings table
-        for index, width in enumerate((95, 145, 75, 140, 100, 120)):
-            self.processingsTable.setColumnWidth(index, width)
-        # Or else let the widget resize the columns automatically
-        # self.dlg.processingsTable.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
         # комбобокс с выбором слоев
-        # self.mMapLayerComboBox.setFilters(QgsMapLayerProxyModel.PolygonLayer) #фильтр полигональных слоев
-        self.VMapLayerComboBox.setFilters(QgsMapLayerProxyModel.HasGeometry)  # фильтр векторных слоев
+        #self.mMapLayerComboBox.setFilters(QgsMapLayerProxyModel.PolygonLayer) #фильтр полигональных слоев
+        self.VMapLayerComboBox.setFilters(QgsMapLayerProxyModel.HasGeometry) #фильтр векторных слоев
         # self.vectorComboBox.setFilters(QgsMapLayerProxyModel.PointLayer) #фильтр точечных слоев
