c9453d12b07b2c2961bc9f5a936a18edf1a7b7ef
Grigory Nedaev <g.nedaev@geoalert.io>
2021-06-15 13:48:12 +0300

    Use QThread & Worker to create processings

--- src/geoalert_master_src/helpers.py
+++ src/geoalert_master_src/helpers.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
-from qgis.core import QgsMapLayerType, QgsWkbTypes
+from qgis.core import QgsMapLayerType, QgsWkbTypes, QgsCoordinateReferenceSystem, QgsGeometry, QgsCoordinateTransform
+
+
+WGS84 = QgsCoordinateReferenceSystem('EPSG:4326')
 
 
 def is_geotiff_layer(layer):
@@ -16,3 +19,21 @@ def is_polygon_layer(layer):
         layer.geometryType() == QgsWkbTypes.PolygonGeometry and
         layer.featureCount() == 1
     )
+
+
+def to_wgs84(geometry, source_crs, transform_context):
+    """"""
+    transform = QgsCoordinateTransform(source_crs, WGS84, transform_context)
+    geometry.transform(transform)
+    return geometry
+
+
+def get_layer_extent(layer, transform_context):
+    """Get a layer's bounding box (extent)."""
+    # Create a geometry from the layer's extent
+    extent_geometry = QgsGeometry.fromRect(layer.extent())
+    # Reproject it to WGS84 if the layer has another CRS
+    layer_crs = layer.crs()
+    if layer_crs != WGS84:
+        extent_geometry = to_wgs84(extent_geometry, layer_crs, transform_context)
+    return extent_geometry
--- src/geoalert_master_src/workers.py
+++ src/geoalert_master_src/workers.py
@@ -1,34 +1,114 @@
+import json
+
 import requests
 from PyQt5.QtCore import QObject, pyqtSignal
 from qgis.core import QgsMessageLog
+
+from .helpers import get_layer_extent, to_wgs84, WGS84
 
 
 class ProcessingFetcher(QObject):
     """"""
     fetched = pyqtSignal(list)
     finished = pyqtSignal()
+    processing_created = False
 
     def __init__(self, url, auth):
         super().__init__()
         self.url = url
         self.auth = auth
 
+    def set_processing_created(self, value):
+        self.processing_created = value
+
     def fetch_processings(self):
         while True:
             if self.thread().isInterruptionRequested():
+                self.processing_created = False
                 self.finished.emit()
                 return
             try:
-                QgsMessageLog.logMessage('FETCHING', 'Mapflow')
                 r = requests.get(self.url, auth=self.auth)
                 r.raise_for_status()
                 processings = r.json()
                 self.fetched.emit(processings)
-                # If there are ongoing processings, spin up a thread again to fetch their state
-                if not [p for p in processings if p['status'] in ("IN_PROGRESS", "UNPROCESSED")]:
+                # If a processing was created during the execution, fetch one more time
+                if self.processing_created:
+                    self.processing_created = False
+                    continue
+                # If there are ongoing processings, keep polling
+                elif [p for p in processings if p['status'] in ("IN_PROGRESS", "UNPROCESSED")]:
+                    self.thread().sleep(5)
+                    continue
+                else:
                     self.finished.emit()
                     break
-                else:
-                    self.thread().sleep(5)
             except Exception as e:
                 QgsMessageLog.logMessage(e, 'Mapflow')
+
+
+class ProcessingCreator(QObject):
+    """"""
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+    tif_uploaded = pyqtSignal(str)
+
+    def __init__(self, processing_name, server, auth, wd, params, transform_context, meta={}, tif=None, aoi_layer=None) -> None:
+        super().__init__()
+        self.processing_name = processing_name
+        self.server = server
+        self.auth = auth
+        self.tif = tif
+        self.wd = wd
+        self.params = params
+        self.meta = meta
+        self.aoi_layer = aoi_layer
+        self.transform_context = transform_context
+
+    def create_processing(self):
+        """Initiate a processing."""
+        if self.thread().isInterruptionRequested():
+            self.finished.emit()
+            return
+        if self.tif:
+            # Upload the image to the server
+            try:
+                with open(self.tif.dataProvider().dataSourceUri(), 'rb') as f:
+                    r = requests.post(f'{self.server}/rest/rasters', auth=self.auth, files={'file': f})
+                r.raise_for_status()
+            except Exception as e:
+                self.error.emit(str(e))
+                return
+            url = r.json()['uri']
+            self.params["url"] = url
+            self.tif_uploaded.emit(url)
+        if self.aoi_layer:
+            aoi = next(self.aoi_layer.getFeatures()).geometry()
+            # Reproject it to WGS84 if the layer has another CRS
+            layer_crs = self.aoi_layer.crs()
+            if layer_crs != WGS84:
+                aoi = to_wgs84(aoi, layer_crs, self.transform_context)
+        else:
+            aoi = get_layer_extent(self.tif, self.transform_context)
+        # If we pass it as JSON, the URL in params will be urlencoded: e.g. ? -> %3F and the creation will fail
+        # To avoid it, we have to convert the body to a string and pass it to the 'data' param instead of 'json'
+        # However, Python serializes a dict with single quotes, so the server will see it as invalid JSON
+        # To fix this, we have to replace '' in the string with ""
+        body = str({
+            "name": self.processing_name,
+            "wdName": self.wd,
+            "geometry": json.loads(aoi.asJson()),
+            "params": self.params,
+            "meta": self.meta
+        }).replace('\'', '"').encode()
+        # Post the processing
+        try:
+            r = requests.post(
+                url=f'{self.server}/rest/processings',
+                headers={'Content-Type': 'application/json'},
+                auth=self.auth,
+                data=body)
+            r.raise_for_status()
+            self.finished.emit()
+        except Exception as e:
+            self.error.emit(str(e))
