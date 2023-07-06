ac4a785c0c4fb77ac46d72bf116e6bf4e552eb23
Alexey Trekin <alexey.trekin@gmail.com>
2023-07-06 13:03:25 +0500

    fix typo

--- mapflow/styles.py
+++ mapflow/styles.py
@@ -14,8 +14,8 @@ DEFAULT_STYLE = "default"
 
 
 def get_style_name(wd_name: str, layer: QgsVectorLayer):
-    name = STYLES.get(wd_name, "default")
-    if name == DEFAULT_STYLE and "class_id" not in layer.fields().names():
+    name = STYLES.get(wd_name, DEFAULT_STYLE)
+    if name == "buildings" and "class_id" not in layer.fields().names():
         name = DEFAULT_STYLE
         # Buildings classes look bad in legend if there are no classes in layer, so we discard the style in this case
     return str(Path(__file__).parent/'static'/'styles'/(name + '.qml'))
