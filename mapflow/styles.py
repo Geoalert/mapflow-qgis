65f80149353813fada54417c864be91e5e78e39b
Alexey Trekin <alexey.trekin@gmail.com>
2023-08-30 17:18:13 +0500

    WIP vector tiles addition instead of file download; no styles; problem with tilejson; no file download yet

--- mapflow/styles.py
+++ mapflow/styles.py
@@ -15,10 +15,13 @@ DEFAULT_STYLE = "default"
 
 def get_style_name(wd_name: str, layer: QgsVectorLayer):
     name = STYLES.get(wd_name, DEFAULT_STYLE)
+    print(name)
+    """
     # Buildings classes look bad in legend if there are no classes in layer, so we discard the style in this case
     if name == "buildings" and "class_id" not in layer.fields().names():
         name = DEFAULT_STYLE
     # Show forest heights for new (updated) forest with block config
     elif name == "forest" and "class_id" in layer.fields().names():
         name = "forest_with_heights"
+    """
     return str(Path(__file__).parent/'static'/'styles'/(name + '.qml'))
