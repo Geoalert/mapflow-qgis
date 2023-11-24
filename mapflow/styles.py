b8a20e669315834cd4bbbe938af94db61289748a
Alexey Trekin <alexey.trekin@gmail.com>
2023-11-24 18:06:20 +0500

    add style for tiled buildings; add old-style file loading; add save geojson; refactor loading results

--- mapflow/styles.py
+++ mapflow/styles.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from qgis.core import QgsVectorLayer
+from qgis.core import QgsVectorLayer, QgsVectorTileLayer
 
 STYLES = {
     '🏠 Buildings': 'buildings',
@@ -14,14 +14,22 @@ DEFAULT_STYLE = "default"
 
 
 def get_style_name(wd_name: str, layer: QgsVectorLayer):
+    if isinstance(layer, QgsVectorTileLayer):
+        return str(Path(__file__).parent/'static'/'styles'/'tiles.qml')
+    else:
+        return get_local_style_name(wd_name)
+
+
+def get_tile_style_name(wd_name):
+    return str(Path(__file__).parent / 'static' / 'styles' / 'tiles.qml')
+
+
+def get_local_style_name(wd_name, layer):
     name = STYLES.get(wd_name, DEFAULT_STYLE)
-    print(name)
-    """
     # Buildings classes look bad in legend if there are no classes in layer, so we discard the style in this case
     if name == "buildings" and "class_id" not in layer.fields().names():
         name = DEFAULT_STYLE
     # Show forest heights for new (updated) forest with block config
     elif name == "forest" and "class_id" in layer.fields().names():
         name = "forest_with_heights"
-    """
     return str(Path(__file__).parent/'static'/'styles'/(name + '.qml'))
