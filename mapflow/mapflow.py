569f09d2ded1ef070b0646d456f5e88c1aaf9273
Alexey Trekin <alexey.trekin@gmail.com>
2024-01-12 14:47:45 +0500

    fix disconnect

--- mapflow/mapflow.py
+++ mapflow/mapflow.py
@@ -2704,7 +2704,8 @@ class Mapflow(QObject):
     def logout(self) -> None:
         """Close the plugin and clear credentials from cache."""
         # disconnect what was connected on login
-        self.dlg.projectsCombo.currentIndexChanged.disconnect(self.project_connection)
+        if self.project_connection is not None:
+            self.dlg.projectsCombo.currentIndexChanged.disconnect(self.project_connection)
         # set token to empty to delete it from settings
         self.settings.setValue('token', '')
         self.processing_fetch_timer.stop()
