c5884674365096900c3f2bf5261e14929dab1ba7
Daryah <d.ignatenko@geoalert.io>
2024-08-07 18:49:14 +0300

    New version of 'mosaic_add' branch without conflicts

--- mapflow/dialogs/main_dialog.py
+++ mapflow/dialogs/main_dialog.py
@@ -408,4 +408,3 @@ class MainDialog(*uic.loadUiType(ui_path/'main_dialog.ui')):
         self.updateProject.setDisabled(is_default)
         self.deleteProject.setToolTip(tooltip)
         self.updateProject.setToolTip(tooltip)
-
