e0832e2579319ee0c993d87a7bc588be395ace40
Alexey Trekin <alexey.trekin@gmail.com>
2026-05-07 01:19:23 +0500

    fix tests

--- tests/functional/test_layer_utils.py
+++ tests/functional/test_layer_utils.py
@@ -2,8 +2,8 @@ from mapflow.functional.layer_utils import *
 
 
 def test_xyz_no_creds():
-    result = generate_xyz_layer_definition('https://xyz.tile.server/{z}/{x}/{y}.png',
-                                           "", "", 18, "xyz")
+    result = generate_xyz_layer_definition(url='https://xyz.tile.server/{z}/{x}/{y}.png',
+                                           username="", password="", max_zoom=18, source_type="xyz")
     assert result == 'type=xyz' \
                      '&url=https://xyz.tile.server/{z}/{x}/{y}.png' \
                      '&zmin=0' \
